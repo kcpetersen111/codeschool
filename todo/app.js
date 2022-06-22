9c9795c48dc330bb0a3df98f3193cd926563f567
Kaleb Petersen <kcpetersen111@gmail.com>
2022-06-22 11:44:37 -0600

    connecting to my backend

--- todo/app.js
+++ todo/app.js
@@ -1,4 +1,5 @@
-var url="http://todo2022.codeschool.cloud";
+// var url="http://todo2022.codeschool.cloud";
+const url="http://localhost:8080";
 
 var app = new Vue({
     el: '#app',
@@ -9,8 +10,13 @@ var app = new Vue({
         descriptionInput:"",
         doneInput:false,
         dateInput: "",
+        newTodoId:"", //this is to turn the most resent thing purple mostly not implemented
         tagsInput:{},
         editingTodo:-1,
+        editingTodoCopy:{},
+        editingTags:[],
+
+        tagFilter:{},
 
     },
     methods: {
@@ -21,11 +27,14 @@ var app = new Vue({
                     tempArr.push(key);
                 }    
             });
-
+            // let dead = this.dateInput;
+            // if (dead ==null){
+            //     dead= new Date();
+            // }
             let todo = {name:this.nameInput,
                 description:this.descriptionInput,
                 done:this.doneInput,
-                deadline:this.dateInput,
+                deadline:this.dateInput, //this.dateInput,
                 tags:tempArr}
 
             // this.todos.push(todo);
@@ -45,15 +54,30 @@ var app = new Vue({
             });
             this.$forceUpdate();
         },
-        updateEditIndex: function(index){
+        updateEditIndex: function(todo, index){
           this.editingTodo =  index;
+          this.editingTodoCopy = {...todo};
+
+        //   if(todo.tags.includes())
+            if( Object.keys(todo).includes('tags')){
+                this.editingTags = [];
+
+                this.tags.forEach(tag =>{
+
+                    this.editingTags.push(todo.tags.includes(tag));
+                });
+            }
         },
+
+        
 
         postTodo: function(newTodo){
             // let newTodo = {
             //     name:"postTodo js",
             //     done:true
             // };
+
+
             fetch(url+"/todo",{
                 method: "POST",
                 body: JSON.stringify(newTodo),
@@ -68,9 +92,48 @@ var app = new Vue({
             });
 
         },
-        // postTodo: function (){
+        putTodo: function(todo_object){
 
-        // }
+            let listOfTags =[];
+
+            this.tags.forEach((tag,index)=>{
+                if(this.editingTags[index]){
+                    listOfTags.push(tag);
+                }
+            });
+            this.editingTodoCopy.tags = [...listOfTags];
+
+
+            fetch(url+"/todo/"+todo_object._id,{
+                method: "Put",
+                body: JSON.stringify(this.editingTodoCopy),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(response =>{
+                response.json().then(data=>{
+                    // console.log(data)
+                    this.newTodoId = data._id
+                    this.refreshList();
+                });
+            });
+
+
+            // this.todos[index] = {...this.editingTodoCopy};
+            this.updateEditIndex({},-1);
+        },
+        deleteTodo: function(todo){
+            fetch(url+"/todo/"+todo._id,{
+                method: "DELETE"
+            }).then(response=>{
+                response.json().then(deleted=>{
+                    console.log(deleted);
+                    this.refreshList();
+                    this.updateEditIndex({},-1);
+
+                })
+            })
+        },
 
         refreshList: function(){
             fetch(url+"/todos").then((response) =>{
@@ -94,9 +157,19 @@ var app = new Vue({
                 this.tags = data;
                 this.tags.forEach(tag => {
                     this.tagsInput[tag] = false;
+                    this.tagFilter[tag] = true ;
                 });
+
+                // this.tags.forEach(element => {
+                    
+                // });
             });
         });
+
+        // this.tags.forEach(element => {
+        //     this.tagFilter.push({element:true});
+        // });
+        
 
     }
 });
